b08bf81ea3d8d101f7bb4cc3548218efd5d100d0
seanpeters86 <sean.peters86@icloud.com>
2017-08-28 09:00:57 -0500

    Add files via upload

---
+++ Legendaries_Argus.bat
@@ -0,0 +1,1 @@
+python simca.py -p st_gear_big_add.simc,st_gear_no_add.simc,st_gear_small_add.simc -f pw,lm,hm -i 300 -t 400 -b 1,2 -c 4 -json -o HTML --resume-mode && python simparser.py -c -s -r -o "legendaries_argus.txt"
